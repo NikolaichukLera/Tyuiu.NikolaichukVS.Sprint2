dff2dd55b776fc45757ec69a8c23ce9fac86a5d7
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-10-13 23:07:57 +0500

    Task1

---
+++ Tyuiu.NikolaichukVS.Sprint2.Task1.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NikolaichukVS.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NikolaichukVS.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9289ade2-37f4-47af-b5e7-6c3dff2c45a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NikolaichukVS.Sprint2.Task1.V6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NikolaichukVS.Sprint2.Task1.V6.Lib;
+
+namespace Tyuiu.NikolaichukVS.Sprint2.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 915;
+            int b = 169;
+            int c = 174;
+            int d = 133;
+
+            bool[] res= new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #2 | Выполнила: Николайчук В. С.| РПСБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Николайчук Валерия Сергеевна | РПСБ-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и и логических операций, а     *");
+            Console.WriteLine("* также арифметических выражений, которая вернет логическую               *");
+            Console.WriteLine("* последовательность(массив): (False, False, True, False, True, False),   *");
+            Console.WriteLine("* при a = 915, b = 169, c = 174, d = 133.                                 *");
+            Console.WriteLine("*False, True, False, True), при x = 123, y = 123                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            Console.WriteLine("a =" + a);
+            Console.WriteLine("b =" + b);
+            Console.WriteLine("c =" + c);
+            Console.WriteLine("d =" + d);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
