2c5a3994ebeba39e81f27b85f5bb1bb6a4f30583
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-10-14 01:05:02 +0500

    Task2

---
+++ Tyuiu.NikolaichukVS.Sprint2.Task2.V18.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NikolaichukVS.Sprint2.Task2.V18.Lib;
+
+namespace Tyuiu.NikolaichukVS.Sprint2.Task2.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int y = 7;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = false;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
