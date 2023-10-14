4cf446c6298e5cda0b418137eb3f80692c4a0fe8
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-10-14 16:50:30 +0500

    Task3

---
+++ Tyuiu.NikolaichukVS.Sprint2.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NikolaichukVS.Sprint2.Task3.V11.Lib;
+
+
+namespace Tyuiu.NikolaichukVS.Sprint2.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = -32;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+
+
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 0.5;
+            Assert.AreEqual(wait, res);
+        }
+
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double res = ds.Calculate(x);
+            double wait = -219.95;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
